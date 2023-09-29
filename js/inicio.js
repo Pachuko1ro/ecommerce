f44dbe784b3a3c5bf01c2bdbc627595d4d9ba730
Pachuko1ro <damiangastol@gastol.com>
2023-09-29 01:25:53 -0300

    0.05

---
+++ js/inicio.js
@@ -0,0 +1,58 @@
+/* ---------------------------------------------------------- */
+/*                     Variables Globales                     */
+/* ---------------------------------------------------------- */
+
+/* ---------------------------------------------------------- */
+/*                     funciones Globales                     */
+/* ---------------------------------------------------------- */
+
+function representarCardsProductos() {
+    
+    var cards = ''
+
+    if(productos.length) {
+        for(var i=0; i<productos.length; i++) {
+            cards += 
+                '<section>' +
+                    '<img src="' + productos[i].foto + '" alt="">' +
+                    '<h4>'+ productos[i].nombre  +'</h4>' +
+                    '<p><b>Stock:</b> '+ productos[i].stock +'</p>' +
+                    '<p><b>Marca:</b> '+ productos[i].marca +'</p>' +
+                    '<p><b>Categoría:</b> '+ productos[i].categoria +'</p>' +
+                    '<p><b>Detalles:</b> '+ productos[i].detalles +'</p>' +
+                    '<br>' +
+                    '<p><b class="precio">Precio: $'+ productos[i].precio +'</b></p>' +
+                    '<p><b style="color:brown;">Envío:</b> '+ (productos[i].envio? 'Si':'No') +'</p>' +
+                    '<button>Agregar 🛒</button>'+
+                '</section>'
+        }
+    }
+    else cards += '<h2>No se encontraron productos para mostrar</h2>'
+
+    document.querySelector('.cards-container').innerHTML = cards
+}
+
+function representarCardsProductos2() {
+    for(var i=0; i<productos.length; i++) {
+        document.querySelector('.cards-container').innerHTML += 
+            '<section>' +
+                '<h4>'+ productos[i].nombre  +'</h4>' +
+                '<img src="' + productos[i].foto + '" alt="">' +
+                '<p><b>Precio:</b> $'+ productos[i].precio +'</p>' +
+                '<p><b>Stock:</b> '+ productos[i].stock +'</p>' +
+                '<p><b>Marca:</b> '+ productos[i].marca +'</p>' +
+                '<p><b>Categoría:</b> '+ productos[i].categoria +'</p>' +
+                '<p><b>Detalles:</b> '+ productos[i].detalles +'</p>' +
+                '<br>' +
+                '<p><b>Envío:</b> '+ (productos[i].envio? 'Si':'No') +'</p>' +
+            '</section>'
+    }
+}
+
+
+function start() {
+    console.warn( document.querySelector('title').innerText )
+
+    representarCardsProductos()
+}
+
---
+++ js/productos.js
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------- */
+/*                     Variables Globales                     */
+/* ---------------------------------------------------------- */
+var productos = [
+    { nombre: 'GUAYMALLEN CHOCO.', precio: 100, stock: 1400, marca: 'Guaymallen', categoria: 'Alfajores', detalles: 'Alfajor simple 38gr. de chocolate.', descripcion: 'Alfajor de chocolate. La caja contiene 40 unidades. Ideal para cumpleaños', envio: true, foto: '/img/guaymachoco.jpg' },
+    { nombre: 'CHOCO. COFLER', precio: 350, stock: 65, marca: 'Arcor', categoria: 'Chocolate', detalles: 'Chocolate COFLER Aireado MIXto x27g.', descripcion: 'Chocolate COFLER Aireado MIXto x27g. Ideal para regalar y compartir.', envio: true, foto: '/img/cofler.jpg' },
+    { nombre: 'FLYNN PAFF', precio: 40, stock: 1000, marca: 'Georgalos', categoria: 'Caramelos', detalles: 'Caramelos FLYNN PAFF Tutti Frutti x70u', descripcion: 'Caramelos FLYNN PAFF Tutti Frutti x70u. Ideal para cumpleaños', envio: true, foto: '/img/flynn.jpg' },
+    { nombre: 'CAR. BILLIKEN', precio: 1340, stock: 23, marca: 'Billiken', categoria: 'Caramelos', detalles: 'Caramelos BILLIKEN FRUTAL 12x600g ', descripcion: 'Caramelos BILLIKEN X 600gr. Ideal para cumpleaños' , envio: true, foto: '/img/billiken.png' },
+    { nombre: 'MOGUL EXTREME', precio: 250, stock: 78, marca: 'Arcor', categoria: 'Gomitas', detalles: 'Mogul Extreme Ositos', descripcion: 'Mogul Extreme Ositos x 50gr. Ideal para cumpleaños' , envio: true, foto: '/img/osito.jpg'  },
+    { nombre: 'GOTITAS DE AMOR', precio: 150, stock: 178, marca: 'Billiken', categoria: 'Pastillas', detalles: 'Pastillas  Gotitas de amor x 12u.', descripcion: 'Pastillas  Gotitas de amor x 12u. Ideal para cumpleaños' , envio: true, foto: '/img/gotita.png'  },
+]
